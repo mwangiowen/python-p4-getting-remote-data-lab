f5d92fecd4826a05c0cbd637aea85eb3b8200616
mwangiowen <owen31588@gmail.com>
2024-01-20 23:56:14 +0300

    Done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,24 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return (
+            response.content
+        )  # Use response.content to get the response body in bytes
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            json_data = json.loads(
+                response_body.decode("utf-8")
+            )  # Decode bytes to UTF-8 before loading JSON
+            return json_data
+        except json.JSONDecodeError as e:
+            print(f"Error loading JSON: {e}")
+            return None
